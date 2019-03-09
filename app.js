3f742c7a608aef7585136fc5cd3a670b65172335
Loki <linkloki@gmail.com>
2019-03-09 18:13:09 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+})
+
+
+
+
+
+
+
+
+app.listen(port);
